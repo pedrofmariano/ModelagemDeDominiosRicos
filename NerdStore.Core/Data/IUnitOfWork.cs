255a65435cff56859e160bd3abd0c074ecd789af
Pedro Henrique Fortunato Silva Mariano <pedro.mariano@localiza.com>
2022-06-06 07:11:10 -0300

    Add project files.

---
+++ NerdStore.Core/Data/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace NerdStore.Core.Data {
+    public interface IUnitOfWork 
+    {
+        Task<bool> Commit();
+    }
+}
---
+++ NerdStore.Core/DomainObjects/DomainException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Core.DomainObjects {
+    public class DomainException: Exception 
+    {
+        public DomainException() 
+        {  }
+
+        //Passa uma menssagem personalizada
+        public DomainException(string message) : base(message) 
+        {  }
+
+        //Passa a mensagem e a Exception interna
+        public DomainException(string message, Exception innerException) : base(message, innerException) 
+        { }
+    }
+}
